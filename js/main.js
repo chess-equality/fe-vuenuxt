52a334874ecd37198d644c46a030649f4b806689
chess-equality <chessequality@gmail.com>
2019-03-27 13:59:22 +0800

    - Conditional rendering

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: "Socks",
-    image: "./assets/vmSocks-green.jpg",
-    description: "Many a pair of socks",
-    altText: "A pair of socks"
+    product: 'Socks',
+    image: './assets/vmSocks-green.jpg',
+    description: 'Many a pair of socks',
+    altText: 'A pair of socks',
+    inStock: true,
+    inventory: 9
   }
 })
