83e78fc739accd3b859d4d0fbbef449e611650ec
chess-equality <chessequality@gmail.com>
2019-03-27 23:34:15 +0800

    - Computed properties

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
+    /*image: './assets/vmSocks-green-onWhite.jpg',*/
+    selectedVariant: 0,
     description: 'Many a pair of socks',
     altText: 'A pair of socks',
-    inStock: false,
+    /*inStock: true,*/
     inventory: 9,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: './assets/vmSocks-green.jpg'
+        variantImage: './assets/vmSocks-green.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: './assets/vmSocks-blue.jpg'
+        variantImage: './assets/vmSocks-blue.jpg',
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -29,9 +33,24 @@ var app = new Vue({
       this.cart += 1
     },
     /*updateProduct: function (variantImage) {*/
-    updateProduct(variantImage) {
+    updateProduct(index) {
+      /*
       console.log('>>>>> variantImage = ' + variantImage)
       this.image = variantImage
+      */
+      console.log('>>>>> index = ' + index)
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image () {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     }
   }
 })
