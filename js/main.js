e89caba53369793df0f905f128d1a7bf1cc6f414
chess-equality <chessequality@gmail.com>
2019-03-27 14:17:50 +0800

    - List rendering

--- js/main.js
+++ js/main.js
@@ -6,6 +6,17 @@ var app = new Vue({
     description: 'Many a pair of socks',
     altText: 'A pair of socks',
     inStock: true,
-    inventory: 9
+    inventory: 9,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 })
