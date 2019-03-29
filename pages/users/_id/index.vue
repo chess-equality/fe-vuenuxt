b3a76714d9ecd39e1353e167f5072fea042a7cd0
chess-equality <chessequality@gmail.com>
2019-03-29 15:05:04 +0800

    - Route with dynamic path

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>A single user, id = {{ $route.params.id }}</h1>
+</template>
+
+<!--
+<script>
+  export default {
+    name: "_id"
+  }
+</script>
+
+<style scoped>
+
+</style>
+-->
---
+++ pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>The users' view</h1>
+</template>
+
+<!--
+<script>
+  export default {
+    name: "users"
+  }
+</script>
+
+<style scoped>
+
+</style>
+-->
